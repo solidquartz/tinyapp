bbfda88d40c40fbab05e1daf5a74126debaf81fd
Aky Mac Donald <kotoko@kotoko.hitronhub.home>
2022-06-18 14:25:24 -0300

    Create helpers.js, move helper functions, add export

--- express-server.js
+++ express-server.js
@@ -17,7 +17,7 @@ app.use(cookieSession({
   keys: ['bubble', 'tea', 'lilac'],
 }));
 
-////////databases//////////
+//////////////databases////////////////
 
 const urlDatabase = {
   sgq3y6: {
@@ -44,58 +44,7 @@ const users = {
   }
 };
 
-////////////////////////
-
-//helper function to generate shortURL
-const generateRandomString = (length = 6) => Math.random().toString(20).substr(2, length);
-
-//helper function to link email with userId
-const getIdFromEmail = (email) => {
-  for (let userId in users) {
-    if (email === users[userId].email) {
-      return userId;
-    }
-  } return null;
-};
-
-//look up email
-const lookUpEmail = (email) => {
-  for (let userId in users) {
-    if (email === users[userId].email) {
-      return users[userId].email;
-    }
-  }
-  return null;
-};
-
-//check if user is logged in
-const checkLogIn = (req, res) => {
-  const userId = req.session.user_id;
-  if (!userId) {
-    res.send("Please log in or register.");
-  } else {
-    for (const user in users) {
-      if (user === userId) {
-        return userId;
-      }
-    }
-    res.redirect("/login");
-  }
-};
-
-//returns the URLS linked to user, in new object
-const urlsForUser = (id, urlDatabase) => {
-  let userURLs = {};
-
-  for (const shortURL in urlDatabase) {
-    if (urlDatabase[shortURL].userID === id) {
-      userURLs[shortURL] = urlDatabase[shortURL];
-    }
-  } return userURLs;
-};
-
-
-//////////////////////////////////
+////////////////////////////////////////
 
 app.get("/", (req, res) => {
   res.send("Hello!");
---
+++ helpers.js
@@ -0,0 +1,51 @@
+////////////////////////
+
+//helper function to generate shortURL
+const generateRandomString = (length = 6) => Math.random().toString(20).substr(2, length);
+
+//helper function to link email with userId
+const getIdFromEmail = (email, users) => { //takes in the users object
+  for (let userId in users) {
+    if (email === users[userId].email) {
+      return userId;
+    }
+  } return null;
+};
+
+//look up email
+const lookUpEmail = (email, users) => {
+  for (let userId in users) {
+    if (email === users[userId].email) {
+      return users[userId].email;
+    }
+  }
+  return null;
+};
+
+//check if user is logged in
+const checkLogIn = (req, res, users) => {
+  const userId = req.session.user_id;
+  if (!userId) {
+    res.send("Please log in or register.");
+  } else {
+    for (const user in users) {
+      if (user === userId) {
+        return userId;
+      }
+    }
+    res.redirect("/login");
+  }
+};
+
+//returns the URLS linked to user, in new object
+const urlsForUser = (id, urlDatabase) => {
+  let userURLs = {};
+
+  for (const shortURL in urlDatabase) {
+    if (urlDatabase[shortURL].userID === id) {
+      userURLs[shortURL] = urlDatabase[shortURL];
+    }
+  } return userURLs;
+};
+
+module.exports = urlsForUser, checkLogIn, lookUpEmail, getIdFromEmail, generateRandomString;
